2bf9d2acf550942d8e838e6c63593af1e2d20387
intunemdmvn <201134654+intunemdmvn@users.noreply.github.com>
2025-07-16 17:58:08 +0700

    Update detection.ps1

--- 7Zip-AutoUpdate/detection.ps1
+++ 7Zip-AutoUpdate/detection.ps1
@@ -1,41 +1,37 @@
 
-# GitHub API URL for the app manifest.
-$apiUrl = "https://api.github.com/repos/microsoft/winget-pkgs/contents/manifests/7/7zip/7zip"
-
-# Fetch version folders then filter only version folders.
-$versions = Invoke-RestMethod -Uri $apiUrl -Headers @{ 'User-Agent' = 'PowerShell' }
-$versionFolders = $versions | Where-Object { $_.type -eq "dir" }
-
-# Extract and sort version numbers then get the latest version.
-$sortedVersions = $versionFolders | ForEach-Object { $_.name } | Sort-Object {[version]$_} -Descending -ErrorAction SilentlyContinue
-$latestVersion = $sortedVersions[0]
+# Get the latest version from the GitHub API URL for the app manifest.
+    $apiUrl = "https://api.github.com/repos/microsoft/winget-pkgs/contents/manifests/7/7zip/7zip"
+    $versions = Invoke-RestMethod -Uri $apiUrl -Headers @{ 'User-Agent' = 'PowerShell' }
+    $versionFolders = $versions | Where-Object { $_.type -eq "dir" }
+    $sortedVersions = $versionFolders | ForEach-Object { $_.name } | Sort-Object {[version]$_} -Descending -ErrorAction SilentlyContinue
+    $latestVersion = $sortedVersions[0]
 
 # Check the installed version number of the app and store it to the $installedVersion variable.
-try {
-    $regPaths = @(
-        "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall",
-        "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
-    )
+    try {
+        $regPaths = @(
+            "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall",
+            "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+        )
 
-    foreach ($regPath in $regPaths) {
-        $apps = Get-ChildItem $regPath -ErrorAction SilentlyContinue
-        foreach ($app in $apps) {
-            $props = Get-ItemProperty $app.PSPath
-            if ($props.DisplayName -like "*7-zip*") {
-                $installedVersion = $($props.DisplayVersion)
+        foreach ($regPath in $regPaths) {
+            $apps = Get-ChildItem $regPath -ErrorAction SilentlyContinue
+            foreach ($app in $apps) {
+                $props = Get-ItemProperty $app.PSPath
+                if ($props.DisplayName -like "*7-zip*") {
+                    $installedVersion = $($props.DisplayVersion)
+                }
             }
         }
+        # If the installed version is greater than or equal the latest version. Return exit code exit 0 and the remediation script won't run.
+        If ($installedVersion -ge $latestVersion){
+            Write-Output "Compliant"
+            exit 0
+        }
+        # Return exit code exit 1 and the remediation script will be executed.
+        Write-Warning "Not Compliant"
+        exit 1
+    } 
+    catch {
+        Write-Warning "Not Compliant"
+        exit 1
     }
-    # If the installed version is greater than or equal the latest version. Return exit code exit 0 and the remediation script won't run.
-    If ($installedVersion -ge $latestVersion){
-        Write-Output "Compliant"
-        exit 0
-    }
-    # Return exit code exit 1 and the remediation script will be executed.
-    Write-Warning "Not Compliant"
-    exit 1
-} 
-catch {
-    Write-Warning "Not Compliant"
-    exit 1
-}

5b62893096346526b3e0442294ad15a0ac3f3cff
intunemdmvn <201134654+intunemdmvn@users.noreply.github.com>
2025-07-16 17:59:15 +0700

    Update remediation.ps1

--- 7Zip-AutoUpdate/remediation.ps1
+++ 7Zip-AutoUpdate/remediation.ps1
@@ -1,38 +1,36 @@
 
-# GitHub API URL for the app manifest.
-$apiUrl = "https://api.github.com/repos/microsoft/winget-pkgs/contents/manifests/7/7zip/7zip"
-
-# Fetch version folders then filter only version folders.
-$versions = Invoke-RestMethod -Uri $apiUrl -Headers @{ 'User-Agent' = 'PowerShell' }
-$versionFolders = $versions | Where-Object { $_.type -eq "dir" }
-
-# Extract and sort version numbers then get the latest version.
-$sortedVersions = $versionFolders | ForEach-Object { $_.name } | Sort-Object {[version]$_} -Descending -ErrorAction SilentlyContinue
-$latestVersion = $sortedVersions[0]
+# Get the latest version from the GitHub API URL for the app manifest.
+    $apiUrl = "https://api.github.com/repos/microsoft/winget-pkgs/contents/manifests/7/7zip/7zip"
+    $versions = Invoke-RestMethod -Uri $apiUrl -Headers @{ 'User-Agent' = 'PowerShell' }
+    $versionFolders = $versions | Where-Object { $_.type -eq "dir" }
+    $sortedVersions = $versionFolders | ForEach-Object { $_.name } | Sort-Object {[version]$_} -Descending -ErrorAction SilentlyContinue
+    $latestVersion = $sortedVersions[0]
 
 # Get contents of the latest version folder then find the .installer.yaml file.
-$latestApiUrl = "$apiUrl/$latestVersion"
-$latestFiles = Invoke-RestMethod -Uri $latestApiUrl -Headers @{ 'User-Agent' = 'PowerShell' }
-$installerFile = $latestFiles | Where-Object { $_.name -like "*.installer.yaml" }
+    $latestApiUrl = "$apiUrl/$latestVersion"
+    $latestFiles = Invoke-RestMethod -Uri $latestApiUrl -Headers @{ 'User-Agent' = 'PowerShell' }
+    $installerFile = $latestFiles | Where-Object { $_.name -like "*.installer.yaml" }
 
 # Download and parse YAML content to get the Url of the latest installer file.
-$yamlUrl = $installerFile.download_url
-$yamlContent = Invoke-RestMethod -Uri $yamlUrl -Headers @{ 'User-Agent' = 'PowerShell' }
-$installerUrl = ($yamlContent -join "`n") -match "InstallerUrl:\s+(http.*)" | ForEach-Object { $Matches[1] }
+    $yamlUrl = $installerFile.download_url
+    $yamlContent = Invoke-RestMethod -Uri $yamlUrl -Headers @{ 'User-Agent' = 'PowerShell' }
+    $yamlString = $yamlContent -join "`n"
+    $installerUrls = [regex]::Matches($yamlString, "InstallerUrl:\s+(http[^\s]+)") | ForEach-Object { $_.Groups[1].Value }
+    $installerUrl = $installerUrls[1]
 
 # Download the latest installer to the temp folder.
-$webClient = [System.Net.WebClient]::new()
-$webClient.DownloadFile($installerUrl, "$env:TEMP\7-zip-latest.exe")
+    $webClient = [System.Net.WebClient]::new()
+    $webClient.DownloadFile($installerUrl, "$env:TEMP\7-zip-latest.exe")
 
 # If the app is running, stop it before processing the update.
-$process = Get-Process -ProcessName *7z* -ErrorAction SilentlyContinue
-if ($process) {
-    $process | Stop-Process -Force
-    Start-Sleep -Seconds 3
-}
+    $process = Get-Process -ProcessName *7z* -ErrorAction SilentlyContinue
+    if ($process) {
+        $process | Stop-Process -Force
+        Start-Sleep -Seconds 3
+    }
 
 # Start the update (install) process.
-Start-Process -FilePath "$env:TEMP\7-zip-latest.exe" -ArgumentList "/S" -Wait
+    Start-Process -FilePath "$env:TEMP\7-zip-latest.exe" -ArgumentList "/S" -Wait
 
 # Cleanup resources.
-Remove-Item -Path "$env:TEMP\7-zip-latest.exe" -Force
+    Remove-Item -Path "$env:TEMP\7-zip-latest.exe" -Force
